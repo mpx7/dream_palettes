17d06eaafeed55163f696283b50a3a421ea449b2
mpx7 <47626572+mpx7@users.noreply.github.com>
2020-07-09 20:48:34 +0900

    Update ran.js

--- ran.js
+++ ran.js
@@ -2,18 +2,18 @@ const a = document.getElementById('disp')
 const jin = "陣内智則"
 let cur = ""
 
-let addchar = function(){
-	let c = jin[ Math.floor(Math.random()*(jin.length)) ]
+const addchar = function(){
+	const c = jin[ Math.floor(Math.random()*(jin.length)) ]
 	cur += c
 	a.appendChild(document.createTextNode(c+" "))
 }
 
-let control = function(){
+const control = function(){
 	addchar()
 	if( cur.endsWith("陣内智則") ){
 		clearInterval(rep)
 	}
 }
 
-const intv = 150
-let rep = setInterval( control, intv )
+const intv = 75
+const rep = setInterval( control, intv )
